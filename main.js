83b072541060b775a7b50f18f2b674586405d142
mharvic <31197516+mharvic@users.noreply.github.com>
2024-11-17 15:54:20 -0700

    Updated Main JS
    Removed contact me button function.

--- main.js
+++ main.js
@@ -24,22 +24,15 @@ if (btn){
 console.log("Button Clicked");
 });
 }
-const button = document.getElementById('btn-num');
-if(button){
-    button.onmouseover = function() {
-    button.innerText = "306-514-8471";
+if(btn){
+    btn.onmouseover = function() {
+    btn.innerText = "GREETINGS";
     };
 
 // Revert the button label when the mouse leaves
-    button.onmouseout = function() {
-    button.innerText = "CONTACT ME";
+    btn.onmouseout = function() {
+    btn.innerText = "CLICK ME";
     };
-
-    button.addEventListener("click", () => {
-      // Navigate to the tel: link
-      window.location.href = 'tel:(306)-514-8471'; // Replace with your phone number
-    });
-
   }
 const orderedList = document.getElementById("numbers");
 
